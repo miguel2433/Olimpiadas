074e4cf087bdcf80436100cf1cd0ab964e5301d3
miguel2433 <miguelitocap12@gmail.com>
2024-10-08 09:56:00 -0300

    api web

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c083e97654b88d2588d79ee0ce3dab594509aef3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b49a34be680ca1b2b53c07d84fc608ca93972db1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
