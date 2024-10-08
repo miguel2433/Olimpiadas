ea9746c7737a15387164c0fc7aa3b79e94cd19fc
ChengLeonardo <leonardo.chenget12de1@gmail.com>
2024-10-08 10:10:00 -0300

    sumar 5to_ al nombre de la base de datos

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6022c7a6f3ef3bca1190f346b65cbca394d97f79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43ab8f076968a90d33dfdc1915e2e023e07f6d7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
